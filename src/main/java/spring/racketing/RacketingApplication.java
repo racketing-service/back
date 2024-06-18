066023eb0e0ab6200384d9b03ffeddff2409737f
huisik-min <82450138+huisik-min@users.noreply.github.com>
2024-06-18 19:26:10 +0900

    chore
    racketing-service/main#1

---
+++ src/main/java/spring/racketing/RacketingApplication.java
@@ -0,0 +1,13 @@
+package spring.racketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/racketing/RacketingApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.racketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
