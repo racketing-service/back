ca51f99b292b06fef1d135f5f7073bee01327b8f
huisik-min <82450138+huisik-min@users.noreply.github.com>
2024-06-19 15:58:59 +0900

    feature - 아이템 리포 기능 개발
    racketing-service/feature#2

--- src/main/java/spring/racketing/repository/ItemRepository.java
+++ src/main/java/spring/racketing/repository/ItemRepository.java
@@ -1,4 +1,36 @@
 package spring.racketing.repository;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import spring.racketing.domain.item.Item;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class ItemRepository {
+
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+
+        } else {
+            // 병합
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
 }
--- src/main/java/spring/racketing/repository/MemberRepository.java
+++ src/main/java/spring/racketing/repository/MemberRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MemberRepository {
 
     private final EntityManager em;
-    
+
     public void save(Member member) {
         em.persist(member);
     }
